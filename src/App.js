eaee47b31143520fc87ea42b17057df3d5e64bec
Max <80323354+GuyBritish@users.noreply.github.com>
2021-08-16 02:32:00 +0700

    Add expenses component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
 	const expenses = [
@@ -30,29 +30,8 @@ function App() {
 
 	return (
 		<div>
-			<h2> First React App </h2>
-			<p> Let's get started! </p>
-
-			<ExpenseItem
-				title={expenses[0].title}
-				amount={expenses[0].amount}
-				date={expenses[0].date}
-			/>
-			<ExpenseItem
-				title={expenses[1].title}
-				amount={expenses[1].amount}
-				date={expenses[1].date}
-			/>
-			<ExpenseItem
-				title={expenses[2].title}
-				amount={expenses[2].amount}
-				date={expenses[2].date}
-			/>
-			<ExpenseItem
-				title={expenses[3].title}
-				amount={expenses[3].amount}
-				date={expenses[3].date}
-			/>
+			<h2> Expense Tracker App </h2>
+			<Expenses expenseList={expenses} />
 		</div>
 	);
 }
