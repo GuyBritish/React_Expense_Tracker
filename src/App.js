d8de7d688cb301dff5c7ffc849bed9302355df53
Max <80323354+GuyBritish@users.noreply.github.com>
2021-08-15 16:50:30 +0700

    Add expense item component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
 	return (
 		<div>
 			<h2> First React App </h2>
 			<p> Let's get started! </p>
+			<ExpenseItem></ExpenseItem>
 		</div>
 	);
 }
