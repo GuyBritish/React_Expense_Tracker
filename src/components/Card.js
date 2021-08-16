f83ad5dd5f365ccf8e38020b08529e7c42ed5189
Max <80323354+GuyBritish@users.noreply.github.com>
2021-08-16 14:33:22 +0700

    add className to custom component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,8 @@
 import "./Card.css";
 
 function Card(props) {
-	return <div className="card"> {props.children} </div>;
+	const classes = "card " + props.className;
+	return <div className={classes}> {props.children} </div>;
 }
 
 export default Card;
