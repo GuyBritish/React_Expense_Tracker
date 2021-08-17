597df2bec3feec47e4e71f83de22829338238619
Max <80323354+GuyBritish@users.noreply.github.com>
2021-08-17 18:53:15 +0700

    Add new expense component

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,13 @@
+import ExpenseForm from "./ExpenseForm";
+
+import "./NewExpense.css";
+
+function NewExpense() {
+	return (
+		<div className="new-expense">
+			<ExpenseForm />
+		</div>
+	);
+}
+
+export default NewExpense;
