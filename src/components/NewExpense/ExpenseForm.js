eb6e01b7317fc235487a598f3face0d113fac996
Max <80323354+GuyBritish@users.noreply.github.com>
2021-08-17 18:58:06 +0700

    Fix className attributes

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -4,17 +4,17 @@ function ExpenseForm() {
 	return (
 		<form>
 			<div className="new-expense__controls">
-				<div className="new-expense__controls">
+				<div className="new-expense__control">
 					<label> Title </label>
 					<input type="text" />
 				</div>
 
-				<div className="new-expense__controls">
+				<div className="new-expense__control">
 					<label> Amount </label>
 					<input type="number" min="0.01" step="0.01" />
 				</div>
 
-				<div className="new-expense__controls">
+				<div className="new-expense__control">
 					<label> Date </label>
 					<input type="date" min="2019-01-01" max="2022-12-31" />
 				</div>
