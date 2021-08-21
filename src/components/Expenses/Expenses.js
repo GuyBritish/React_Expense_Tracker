b8c6ca626cbe3124d3a2fe36ee00e2885f09fc94
Max <80323354+GuyBritish@users.noreply.github.com>
2021-08-21 19:08:48 +0700

    Use list of expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -17,26 +17,13 @@ function Expenses(props) {
 		<div>
 			<Card className="expenses">
 				<ExpenseFilter selected={filterYear} onFilterYear={yearFilterHandler} />
-				<ExpenseItem
-					title={props.expenseList[0].title}
-					amount={props.expenseList[0].amount}
-					date={props.expenseList[0].date}
-				/>
-				<ExpenseItem
-					title={props.expenseList[1].title}
-					amount={props.expenseList[1].amount}
-					date={props.expenseList[1].date}
-				/>
-				<ExpenseItem
-					title={props.expenseList[2].title}
-					amount={props.expenseList[2].amount}
-					date={props.expenseList[2].date}
-				/>
-				<ExpenseItem
-					title={props.expenseList[3].title}
-					amount={props.expenseList[3].amount}
-					date={props.expenseList[3].date}
-				/>
+				{props.expenseList.map((expense) => (
+					<ExpenseItem
+						title={expense.title}
+						amount={expense.amount}
+						date={expense.date}
+					/>
+				))}
 			</Card>
 		</div>
 	);
