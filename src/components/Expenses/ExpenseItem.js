a929b02cfa06d36879cd7a8c1a919ebb3680abb9
Max <80323354+GuyBritish@users.noreply.github.com>
2021-08-22 03:07:56 +0700

    Add li tag

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -14,15 +14,17 @@ function ExpenseItem(props) {
 	};
 
 	return (
-		<Card className="expense-item">
-			<ExpenseDate date={props.date} />
-			<div className="expense-item__description">
-				<h2> {props.title} </h2>
-				<div className="expense-item__price"> ${props.amount} </div>
-			</div>
+		<li>
+			<Card className="expense-item">
+				<ExpenseDate date={props.date} />
+				<div className="expense-item__description">
+					<h2> {props.title} </h2>
+					<div className="expense-item__price"> ${props.amount} </div>
+				</div>
 
-			<button onClick={clickHandler}> Change Title </button>
-		</Card>
+				<button onClick={clickHandler}> Change Title </button>
+			</Card>
+		</li>
 	);
 }
 
