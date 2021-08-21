26cdf368f6499c701a3c6b78a71e21c73e25419e
Max <80323354+GuyBritish@users.noreply.github.com>
2021-08-21 23:36:42 +0700

    Print new title

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -10,13 +10,14 @@ function ExpenseItem(props) {
 
 	const clickHandler = () => {
 		setTitle("Clicked!");
+		console.log(title);
 	};
 
 	return (
 		<Card className="expense-item">
 			<ExpenseDate date={props.date} />
 			<div className="expense-item__description">
-				<h2> {title} </h2>
+				<h2> {props.title} </h2>
 				<div className="expense-item__price"> ${props.amount} </div>
 			</div>
 
